6382cc7717b4e6214eb9d0911d21881c595e06bd
Chris Free <cfree@Chriss-MacBook-Pro-2.local>
2021-12-14 07:36:08 -0800

    converted to package and added data documentation

---
+++ R/data.R
@@ -0,0 +1,40 @@
+#' Aquatic Foods Composition Database (AFCD) reference key
+#'
+#' Reference key for the Aquatic Foods Composition Database (AFCD). The AFCD utilizes a mixture of values from Food Composition Tables (FCTs) and from the peer-reviewed literature.
+#'
+#' @format A data frame with the following attributes::
+#' \describe{
+#'   \item{study_type}{Study type (Peer-reviewed literature or FCT table)}
+#'   \item{study_id}{Study id}
+#'   \item{citation}{Citation}
+#'   \item{doi}{DOI for peer-reviewed sources and dataset link for FCT sources}
+#'   \item{region}{Study region (peer-reviewed only)}
+#'   \item{database}{Nuutrient database (FCT sources only)}
+#'   \item{units}{Nutrients per unit (FCT sources only)}
+#' }
+#' @source Golden CD, Koehn JZ, Shepon A, Passarelli S, Free CM, Viana DF, Matthey H, Eurich JG, Gephart JA, Fluet-Chouinnard E, Nyboer EA, Lynch AJ, Kjellevold M, Bromage S, Charlebois P, Barange M, Vannuccini S, Cao L, Kleisner KM, Rimm EB, Danaei G, DeSisto C, Kelahan H, Fiorella KJ, Little DC, Allison EH, Fanzo J, Thilsted SH (2021) Aquatic foods to nourish nations. _Nature_ 598: 315-320.
+"afcd_refs"
+
+#' Aquatic Foods Composition Database (AFCD)
+#'
+#' A cleaned version of the Aquatic Foods Composition Database (AFCD).
+#'
+#' @format A data frame with the following attributes::
+#' \describe{
+#'   \item{sciname}{Scientific name}
+#'   \item{study_id}{Study id}
+#'   \item{iso3}{ISO3 of source country(s)}
+#'   \item{fao3}{}
+#'   \item{prod_catg}{Production category (farmed, wild capture, unknown)}
+#'   \item{food_part}{Part of food}
+#'   \item{food_prep}{Preparation of food}
+#'   \item{food_name}{Name of food, in English}
+#'   \item{food_name_orig}{Name of food, in original data}
+#'   \item{notes}{Notes}
+#'   \item{nutrient}{Nuteient}
+#'   \item{description}{Description of nutrient}
+#'   \item{units}{Units per 100g of edible food portion}
+#'   \item{value}{Value}
+#' }
+#' @source Golden CD, Koehn JZ, Shepon A, Passarelli S, Free CM, Viana DF, Matthey H, Eurich JG, Gephart JA, Fluet-Chouinnard E, Nyboer EA, Lynch AJ, Kjellevold M, Bromage S, Charlebois P, Barange M, Vannuccini S, Cao L, Kleisner KM, Rimm EB, Danaei G, DeSisto C, Kelahan H, Fiorella KJ, Little DC, Allison EH, Fanzo J, Thilsted SH (2021) Aquatic foods to nourish nations. _Nature_ 598: 315-320.
+"afcd"
--- data-raw/Step1_format_afcd.R
+++ data-raw/Step1_format_afcd.R
@@ -9,8 +9,8 @@ rm(list = ls())
 library(tidyverse)
 
 # Directories
-indir <- "/Users/cfree/Dropbox/Chris/UCSB/projects/nutrition/AFCD/raw/"
-outdir <- "/Users/cfree/Dropbox/Chris/UCSB/projects/nutrition/AFCD/processed"
+indir <- "data-raw/raw"
+outdir <- "data-raw/processed"
 
 # Resources
 # GitHub: https://github.com/zachkoehn/aquatic_foods_nutrient_database
@@ -38,17 +38,17 @@ col_key_orig <- readxl::read_excel(file.path(indir, "afcd_variable_codex.xlsx"))
 ################################################################################
 
 # Format FCT reference key
-ref_fct <- ref_fct_orig %>% 
+ref_fct <- ref_fct_orig %>%
   # Rename
-  janitor::clean_names() %>% 
-  rename(study_id=study_id_number, 
+  janitor::clean_names() %>%
+  rename(study_id=study_id_number,
          doi=link_to_dataset,
          database=nutrient_database,
-         units=nutrients_per) %>% 
+         units=nutrients_per) %>%
   # Add study type
-  mutate(study_type="Food Composition Table (FCT)") %>% 
+  mutate(study_type="Food Composition Table (FCT)") %>%
   # Arrange
-  select(study_type, study_id, citation, doi, database, units, everything()) %>% 
+  select(study_type, study_id, citation, doi, database, units, everything()) %>%
   # Remove useless columns
   select(-c(notes, added_by, already_included, format))
 
@@ -57,19 +57,19 @@ colnames(ref_fct)
 table(ref_fct$units)
 
 # Format peer reviewed reference key
-ref_peer <- ref_peer_orig %>% 
+ref_peer <- ref_peer_orig %>%
   # Rename
-  janitor::clean_names() %>% 
+  janitor::clean_names() %>%
   rename(study_id=study_id_number,
          doi=study_doi,
          region=study_region,
-         citation=study_apa_citation) %>% 
+         citation=study_apa_citation) %>%
   # Add study type
-  mutate(study_type="Peer-reviewed literature") %>% 
+  mutate(study_type="Peer-reviewed literature") %>%
   # Convert study id
-  mutate(study_id=as.character(study_id)) %>% 
+  mutate(study_id=as.character(study_id)) %>%
   # Arrange
-  select(study_type, study_id, everything()) %>% 
+  select(study_type, study_id, everything()) %>%
   # Remove useless columns
   select(-x5)
 
@@ -78,8 +78,8 @@ colnames(ref_peer)
 table(ref_peer$region)
 
 # Merge reference key
-ref_key <- bind_rows(ref_peer, ref_fct) %>% 
-  arrange(study_type, study_id) %>% 
+ref_key <- bind_rows(ref_peer, ref_fct) %>%
+  arrange(study_type, study_id) %>%
   select(study_type, study_id, citation, everything())
 
 # Inspect
@@ -93,105 +93,105 @@ saveRDS(ref_key, file.path(outdir, "AFCD_reference_key.Rds"))
 ################################################################################
 
 # Build column key
-col_key <- col_key_orig %>% 
+col_key <- col_key_orig %>%
   # Rename
-  janitor::clean_names() %>% 
+  janitor::clean_names() %>%
   rename(col_id=x1, col_name=afcd_variable_name, units=unit, fao_code=fao_tagname_if_applicable)
 
 # Build nutrient key
-nutr_key <- col_key %>% 
+nutr_key <- col_key %>%
   # Simplify
-  select(-col_id) %>% 
+  select(-col_id) %>%
   # Reduce to nutrients
-  filter(units!="none" | is.na(units)) %>% 
+  filter(units!="none" | is.na(units)) %>%
   # Rename
-  rename(nutrient_orig=col_name) %>% 
+  rename(nutrient_orig=col_name) %>%
   # Format
-  mutate(nutrient_orig=tolower(nutrient_orig)) %>% 
-  # Add nutrient column 
-  mutate(nutrient=stringr::str_to_title(nutrient_orig) %>% gsub("_", " ", .)) %>% 
+  mutate(nutrient_orig=tolower(nutrient_orig)) %>%
+  # Add nutrient column
+  mutate(nutrient=stringr::str_to_title(nutrient_orig) %>% gsub("_", " ", .)) %>%
   # Arrange
   select(nutrient_orig, nutrient, units, fao_code, description)
 
 
-  
+
 
 
 # Format data
 ################################################################################
 
 # Format data
-data1 <- data_orig %>% 
+data1 <- data_orig %>%
   # Rename columns
-  janitor::clean_names() %>% 
+  janitor::clean_names() %>%
   rename(sciname=taxa_name,
-         food_part=parts_of_food, 
+         food_part=parts_of_food,
          food_prep=preparation_of_food,
          prod_catg=production_category,
          edible_prop=edible_portion_coefficient,
          study_id=study_id_number,
          iso3=country_iso3,
-         fao3=fao_3a_code, 
+         fao3=fao_3a_code,
          fct_code_orig=original_fct_food_code,
          food_name=food_name_in_english,
-         food_name_orig=food_name_in_original_language) %>% 
+         food_name_orig=food_name_in_original_language) %>%
   # Arrange
-  select(sciname:food_name_orig, notes, everything()) %>% 
+  select(sciname:food_name_orig, notes, everything()) %>%
   # Gather
-  gather(key="nutrient", value="value", 22:ncol(.)) %>% 
+  gather(key="nutrient", value="value", 22:ncol(.)) %>%
   # Reduce to rows with data
   filter(!is.na(value))
 
 # Inspect countries
-contry_key <- data1 %>% 
+contry_key <- data1 %>%
   # Unique ISOs
-  select(iso3) %>% 
-  unique() %>% 
+  select(iso3) %>%
+  unique() %>%
   # Add country
-  mutate(country=countrycode::countrycode(iso3, "iso3c", "country.name")) %>% 
+  mutate(country=countrycode::countrycode(iso3, "iso3c", "country.name")) %>%
   # Sort
   arrange(iso3)
 
 # Format data some more
-data2 <- data1 %>% 
+data2 <- data1 %>%
   # Format scientific name
-  mutate(sciname=stringr::str_to_sentence(sciname), 
-         sciname=stringr::str_trim(sciname)) %>% 
+  mutate(sciname=stringr::str_to_sentence(sciname),
+         sciname=stringr::str_trim(sciname)) %>%
   # Format other taxonomic info
   mutate(across(.cols=kingdom:genus, .fns=stringr::str_to_title),
-         across(.cols=kingdom:genus, .fns=stringr::str_trim)) %>% 
+         across(.cols=kingdom:genus, .fns=stringr::str_trim)) %>%
   # Format taxa database
-  mutate(taxa_db=stringr::str_to_upper(taxa_db)) %>% 
+  mutate(taxa_db=stringr::str_to_upper(taxa_db)) %>%
   # Format food parts
-  mutate(food_part=gsub("_", " ",  food_part)) %>% 
+  mutate(food_part=gsub("_", " ",  food_part)) %>%
   # Format food preparation
-  mutate(food_prep=gsub("_", " ",  food_prep)) %>% 
+  mutate(food_prep=gsub("_", " ",  food_prep)) %>%
   # Format production category
-  mutate(prod_catg=gsub("_", " ",  prod_catg)) %>% 
+  mutate(prod_catg=gsub("_", " ",  prod_catg)) %>%
   # Add nutrient info
-  left_join(nutr_key, by=c("nutrient"="nutrient_orig")) %>% 
+  left_join(nutr_key, by=c("nutrient"="nutrient_orig")) %>%
   # Arrange
-  select(sciname:taxa_db, 
+  select(sciname:taxa_db,
          study_id, peer_review, iso3, fao3,
          prod_catg, food_part, food_prep, food_name, food_name_orig, notes,
-         nutrient, description, fao_name, units, value, everything())
-  
-  
+         nutrient, description, fao_code, units, value, everything())
+
+
 # Inspect
 str(data2)
 freeR::complete(data2)
 
 # Inspect nutrients
-nutr_key_check <- data2 %>% 
-  select(nutrient, units, description) %>% 
+nutr_key_check <- data2 %>%
+  select(nutrient, units, description) %>%
   unique()
 
 # Inspect taxa
 table(data2$kingdom)
 sort(unique(data2$phylum))
-sort(unique(data2$order))  
-sort(unique(data2$family))  
-sort(unique(data2$genus)) 
+sort(unique(data2$order))
+sort(unique(data2$family))
+sort(unique(data2$genus))
 table(data2$taxa_db)
 
 # Inspect food parts
@@ -222,30 +222,26 @@ sort(unique(data2$food_name_orig))
 ################################################################################
 
 # Species key
-spp_key <- data2 %>% 
+spp_key <- data2 %>%
   # Unique species
-  group_by(sciname, taxa_id) %>% 
-  summarize(n=n()) %>% 
-  ungroup() %>% 
+  group_by(sciname, taxa_id) %>%
+  summarize(n=n()) %>%
+  ungroup() %>%
   # Recode species
-  rename(sciname_orig=sciname) %>% 
+  rename(sciname_orig=sciname) %>%
   mutate(sciname=)
 
 # Check names
-wrong_names1 <- freeR::check_names(spp_key$sciname)
-wrong_names2 <- wrong_names1[!grepl("spp|sp.", wrong_names1) & wrong_names1!=""]
-suggested_names <- freeR::suggest_names(wrong_names2[1:10])
-
-
-
-prep_suggested_names <- function(suggest_list){}
+# wrong_names1 <- freeR::check_names(spp_key$sciname)
+# wrong_names2 <- wrong_names1[!grepl("spp|sp.", wrong_names1) & wrong_names1!=""]
+# suggested_names <- freeR::suggest_names(wrong_names2[1:10])
 
 
 # Export data
 ################################################################################
 
 # Export data
-saveRDS(data2, file=file.path(outdir, "20210914_AFCD_cleaned.Rds"))
+saveRDS(data2, file=file.path(outdir, "AFCD_data.Rds"))
 
 
 
