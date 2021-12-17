98e3539f81c37beeed3a8ebe520124080c803190
Chris Free <cfree14@gmail.com>
2021-12-16 16:24:12 -0800

    build new datasets

--- R/data.R
+++ R/data.R
@@ -1,26 +1,66 @@
-#' Aquatic Foods Composition Database (AFCD)
+#' Aquatic Foods Composition Database (AFCD) - scientific names
 #'
-#' A cleaned version of the Aquatic Foods Composition Database (AFCD).
+#' A cleaned version of the Aquatic Foods Composition Database (AFCD). This dataset includes observations with foods identified by scientific names.
 #'
 #' @format A data frame with the following attributes:
 #' \describe{
-#'   \item{sciname}{Scientific name}
+#'   \item{sciname}{Scientific name, harmonized}
+#'   \item{sciname_orig}{Scientific name, in the raw AFCD}
+#'   \item{taxa_type}{Taxa type, species or a group of species}
+#'   \item{taxa_level}{Taxa level (species, genus, family, order, other)}
+#'   \item{study_type}{Study type (peer-reviewed paper or FCT report)}
 #'   \item{study_id}{Study id}
 #'   \item{iso3}{ISO3 of source country(s)}
-#'   \item{fao3}{}
+#'   \item{country}{Source country(s)}
 #'   \item{prod_catg}{Production category (farmed, wild capture, unknown)}
 #'   \item{food_part}{Part of food}
 #'   \item{food_prep}{Preparation of food}
 #'   \item{food_name}{Name of food, in English}
 #'   \item{food_name_orig}{Name of food, in original data}
+#'   \item{fct_code_orig}{FCT code, in original data}
+#'   \item{food_id}{Food id}
+#'   \item{edible_prop}{Edible proportion}
 #'   \item{notes}{Notes}
-#'   \item{nutrient}{Nuteient}
-#'   \item{description}{Description of nutrient}
-#'   \item{units}{Units per 100g of edible food portion}
+#'   \item{nutrient_type}{Nutient type}
+#'   \item{nutrient}{Nutient name}
+#'   \item{nutrient}{Nutient name, in the raw AFCD}
+#'   \item{nutrient_desc}{Description of nutrient}
+#'   \item{nutrient_units}{Units per 100g of edible food portion}
 #'   \item{value}{Value}
 #' }
 #' @source Golden CD, Koehn JZ, Shepon A, Passarelli S, Free CM, Viana DF, Matthey H, Eurich JG, Gephart JA, Fluet-Chouinnard E, Nyboer EA, Lynch AJ, Kjellevold M, Bromage S, Charlebois P, Barange M, Vannuccini S, Cao L, Kleisner KM, Rimm EB, Danaei G, DeSisto C, Kelahan H, Fiorella KJ, Little DC, Allison EH, Fanzo J, Thilsted SH (2021) Aquatic foods to nourish nations. Nature 598: 315-320.
-"afcd"
+"afcd1"
+
+#' Aquatic Foods Composition Database (AFCD) - w/out scientific names
+#'
+#' A cleaned version of the Aquatic Foods Composition Database (AFCD). This dataset includes observations with foods identified by general names (not scientific names).
+#'
+#' @format A data frame with the following attributes:
+#' \describe{
+#'   \item{taxa_name_source}{English or original food name}
+#'   \item{taxa_name}{Food name}
+#'   \item{study_type}{Study type (peer-reviewed paper or FCT report)}
+#'   \item{study_id}{Study id}
+#'   \item{iso3}{ISO3 of source country(s)}
+#'   \item{country}{Source country(s)}
+#'   \item{prod_catg}{Production category (farmed, wild capture, unknown)}
+#'   \item{food_part}{Part of food}
+#'   \item{food_prep}{Preparation of food}
+#'   \item{food_name}{Name of food, in English}
+#'   \item{food_name_orig}{Name of food, in original data}
+#'   \item{fct_code_orig}{FCT code, in original data}
+#'   \item{food_id}{Food id}
+#'   \item{edible_prop}{Edible proportion}
+#'   \item{notes}{Notes}
+#'   \item{nutrient_type}{Nutient type}
+#'   \item{nutrient}{Nutient name}
+#'   \item{nutrient}{Nutient name, in the raw AFCD}
+#'   \item{nutrient_desc}{Description of nutrient}
+#'   \item{nutrient_units}{Units per 100g of edible food portion}
+#'   \item{value}{Value}
+#' }
+#' @source Golden CD, Koehn JZ, Shepon A, Passarelli S, Free CM, Viana DF, Matthey H, Eurich JG, Gephart JA, Fluet-Chouinnard E, Nyboer EA, Lynch AJ, Kjellevold M, Bromage S, Charlebois P, Barange M, Vannuccini S, Cao L, Kleisner KM, Rimm EB, Danaei G, DeSisto C, Kelahan H, Fiorella KJ, Little DC, Allison EH, Fanzo J, Thilsted SH (2021) Aquatic foods to nourish nations. Nature 598: 315-320.
+"afcd2"
 
 #' Aquatic Foods Composition Database (AFCD) nutrient key
 #'
--- data-raw/Step4_add_data_to_package.R
+++ data-raw/Step4_add_data_to_package.R
@@ -13,12 +13,14 @@ indir <- "data-raw/raw"
 outdir <- "data-raw/processed"
 
 # Read data
-afcd <- readRDS(file=file.path(outdir, "AFCD_data.Rds"))
+afcd1 <- readRDS(file=file.path(outdir, "AFCD_data_sci.Rds"))
+afcd2 <- readRDS(file=file.path(outdir, "AFCD_data_comm.Rds"))
 afcd_refs <- readRDS(file=file.path(outdir, "AFCD_reference_key.Rds"))
 afcd_nutrients <- readRDS(file=file.path(outdir, "AFCD_nutrient_key.Rds"))
 
 
 # Export data
-usethis::use_data(afcd, overwrite = T)
+usethis::use_data(afcd1, overwrite = T)
+usethis::use_data(afcd2, overwrite = T)
 usethis::use_data(afcd_refs, overwrite = T)
 usethis::use_data(afcd_nutrients, overwrite = T)
